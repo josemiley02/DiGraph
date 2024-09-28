d58ea8af12b46b118431eebf220306d112cb0b2f
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-09-28 00:43:36 -0400

    First commit for Vertex class

---
+++ Vertex/Vertex.cs
@@ -0,0 +1,33 @@
+namespace DiGraph.Vertex;
+
+public class Vertex<T>
+{
+    public Vertex(T value, int idVertex)
+    {
+        Value = value;
+        IdVertex = idVertex;
+    }
+
+    public T Value { get; set; }
+    public int InDegree { get; set; }
+    public int OutDegree { get; set; }
+    public int IdVertex {get; set;}
+
+    public override string ToString()
+    {
+        return $"Vertex:{IdVertex}, Value: {Value!.ToString()}";
+    }
+    public override bool Equals(object? obj)
+    {
+        if(obj is Vertex<T>)
+        {
+            var v = obj as Vertex<T>;
+            return Value!.Equals(v!.Value) && IdVertex == v.IdVertex;
+        }
+        throw new ArgumentException("");
+    }
+    public override int GetHashCode()
+    {
+        return IdVertex;
+    }
+}
